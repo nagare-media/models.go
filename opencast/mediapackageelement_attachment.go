98fa97a2d4802222d037ffbee0a3c6104483d47d
Matthias Neugebauer <mtneug@shio.solutions>
2025-01-08 17:00:23 +0100

    Update year

--- opencast/mediapackageelement_attachment.go
+++ opencast/mediapackageelement_attachment.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021-2024 The nagare media authors
+Copyright 2021-2025 The nagare media authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
